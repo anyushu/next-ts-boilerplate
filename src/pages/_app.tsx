3013e5849b5cbe6b271ef6be7b7f5e81528f8ddf
anyushu <suzuki@lamp.jp>
2022-02-08 19:08:00 +0900

    change directory

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import 'styles/globals.css'
 import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
-import { defaultSeo } from 'next-seo.config'
+import { defaultSeo } from 'libs/next-seo.config'
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
