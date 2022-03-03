fd3324702c3a33a1115f839aded74105ca214098
anyushu <suzuki@lamp.jp>
2022-03-03 14:43:38 +0900

    change directory

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
-import { defaultSeo } from '@/libs/next-seo.config'
+import { defaultSeo } from '@/utils/next-seo.config'
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
--- src/utils/next-seo.config.ts
+++ src/utils/next-seo.config.ts
@@ -5,6 +5,7 @@ export const siteTitle = process.env.NEXT_PUBLIC_SITE_NAME || 'site name'
 
 /**
  * next-seo defaultS config
+ * @see https://github.com/garmeeh/next-seo#default-seo-configuration
  */
 export const defaultSeo = {
   defaultTitle: siteTitle,
